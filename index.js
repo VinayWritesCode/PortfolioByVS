17aa9c27a4405df705413a5edb20303e9cc01bb0
VinayWritesCode <vinaysuhalka49@gmail.com>
2022-01-17 19:52:17 +0530

    Added some changes in MenuBar

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+let menuBar = ""; 
+
+const sidebar = (x) =>{
+  let sidebar = document.querySelector("#sidebar");
+  if (sidebar.style.marginLeft === "0vw") {
+    sidebar.style.marginLeft = "-40vw";
+    x.style.display = "inline-block";
+  }
+  else {
+    sidebar.style.marginLeft = "0vw";
+    x.style.display = "none";
+  }
+}
 const toggleMenu = (x) => {
-  
   x.classList.toggle("change");
+  menuBar = x;
+  sidebar(x);
   console.log("hello, this is togglemenu");
+}
+
+const toggleMenuInside = ()=>{
+  toggleMenu(menuBar);
 }
 
